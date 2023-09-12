41c75c64ea86885bdc4de6cd1cc2064f61999147
mrxmit <tonixsmit@gmail.com>
2023-09-12 15:10:08 +0300

    add all project files

---
+++ data/students-data.js
@@ -0,0 +1,9 @@
+const students = [
+    { name: 'Bao ', eliminated: false, _id: 125223, sex: 'female' },
+    { name: 'Happy', eliminated: true, _id: 127904, sex: 'male' },
+    { name: 'Toni', eliminated: false, _id: 139608, sex: 'male' },
+]
+
+export {
+    students
+}
